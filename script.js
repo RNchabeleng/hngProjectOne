6794c79c7df9eb0b1861c42e7e6531327cb157a3
Riki Nchabeleng <rikinchabeleng@gmail.com>
2023-09-07 18:00:01 +0200

    Added styling and refactored js

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
     // Get elements by their data-testid attributes
-    const slackUserName = document.querySelector('[data-testid="slackUserName"]');
-    const slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
     const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
-    const myTrack = document.querySelector('[data-testid="myTrack"]');
-    const githubURL = document.querySelector('[data-testid="githubURL"]');
     
     // Display the current day of the week
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
     const currentDay = daysOfWeek[currentDate.getDay()];
-    currentDayOfTheWeek.textContent = currentDay;
+    currentDayOfTheWeek.textContent = `Today is: ${currentDay}`;
     
-    // Update the current UTC time in real-time
-    function updateUTCTime() {
-        const currentUTCTimeValue = new Date().toISOString();
-        currentUTCTime.textContent = currentUTCTimeValue;
+    // Update the real-time counter with milliseconds
+    function updateRealTimeCounter() {
+        const currentTime = new Date();
+        const milliseconds = currentTime.getMilliseconds();
+        const formattedTime = `${currentTime.toISOString().split('T')[1].split('.')[0]}.${milliseconds}`;
+        currentUTCTime.textContent = `Time (UTC): ${formattedTime}`;
     }
     
-    // Update the UTC time every second
-    setInterval(updateUTCTime, 1000);
+    // Update the real-time counter every millisecond
+    setInterval(updateRealTimeCounter, 1);
     
-    // Set your track
-    myTrack.textContent = "Frontend";
-    
-    // Set your GitHub repository URL
-    githubURL.href = "https://github.com/yourusername/your-repo";
-    
-    // Initially update the UTC time
-    updateUTCTime();
+    // Initially update the real-time counter
+    updateRealTimeCounter();
 });
+
