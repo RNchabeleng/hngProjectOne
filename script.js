5cc032be64c6d91f389584dd903f8ce8fd425d69
Riki Nchabeleng <rikinchabeleng@gmail.com>
2023-09-07 18:26:11 +0200

    refactoring js

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Get elements by their data-testid attributes
     const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
     
-    // Display the current day of the week
+    // Display the current day
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
-    const currentDay = daysOfWeek[currentDate.getDay()];
-    currentDayOfTheWeek.textContent = `Today is: ${currentDay}`;
+    currentDayOfTheWeek.textContent = `Today is: ${daysOfWeek[currentDate.getDay()]}`;
     
-    // Update the real-time counter with milliseconds
+    // Update counter with milliseconds
     function updateRealTimeCounter() {
         const currentTime = new Date();
-        const milliseconds = currentTime.getMilliseconds();
-        const formattedTime = `${currentTime.toISOString().split('T')[1].split('.')[0]}.${milliseconds}`;
-        currentUTCTime.textContent = `Time (UTC): ${formattedTime}`;
+        currentUTCTime.textContent = `Time (UTC): ${currentTime.toISOString().substr(11, 12)}`;
     }
     
-    // Update the real-time counter every millisecond
+    // Update counter every millisecond
     setInterval(updateRealTimeCounter, 1);
-    
-    // Initially update the real-time counter
+
     updateRealTimeCounter();
 });
-
