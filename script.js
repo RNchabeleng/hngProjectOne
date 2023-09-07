73f69bb9ce20655b103a06e5207663b77d18f0b1
Riki Nchabeleng <rikinchabeleng@gmail.com>
2023-09-07 22:08:03 +0200

    refactoring mistakes

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
-    
-    // Display the current day
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDate = new Date();
-    currentDayOfTheWeek.textContent = `Today is: ${daysOfWeek[currentDate.getDay()]}`;
-    
-    // Update counter with milliseconds
-function updateRealTimeCounter() {
-    const currentTime = new Date();
-    const utcTimeMilliseconds = currentTime.getTime();
-    currentUTCTime.textContent = `Time (UCT) in milliseconds: ${utcTimeMilliseconds}`;
-}
 
-    setInterval(updateRealTimeCounter, 1);
+    // Update the UTC time once
+    function updateCurrentUTCTime() {
+        const currentTime = new Date();
+        const utcTimeMilliseconds = currentTime.getTime();
+        currentUTCTime.textContent = `${utcTimeMilliseconds}`;
+    }
 
-    updateRealTimeCounter();
+    // Call the function to update the time once when the DOM is ready
+    updateCurrentUTCTime();
 });
+
