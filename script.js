afe1a1f0f5c03f327ec37168fcacc4cf00a71ab2
Riki Nchabeleng <rikinchabeleng@gmail.com>
2023-09-07 22:35:55 +0200

    refacotoring

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
-
-    // Update the UTC time once
-    function updateCurrentUTCTime() {
+    
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+    currentDayOfTheWeek.textContent = `${daysOfWeek[currentDate.getUTCDay()]}`;
+    
+    function updateRealTimeCounter() {
         const currentTime = new Date();
         const utcTimeMilliseconds = currentTime.getTime();
         currentUTCTime.textContent = `${utcTimeMilliseconds}`;
     }
-
-    // Call the function to update the time once when the DOM is ready
-    updateCurrentUTCTime();
+    
+    updateRealTimeCounter();
 });
-
